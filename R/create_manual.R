87d3d5c5549ed5282f834e41daef693ff70c17fd
Unknown <marcel.schliebs@sciencespo.fr>
2017-12-18 16:24:53 +0100

    Finished manual

--- R/create_manual.R
+++ R/create_manual.R
@@ -1,4 +1,6 @@
- pack <- "voteR"
+#!!!! https://tex.stackexchange.com/questions/125274/error-font-ts1-zi4r-at-540-not-found !!!
+
+pack <- "voteR"
  path <- "C:/Users/Schliebs/OneDrive/github/voteR" #getwd()#find.package(pack)#
 
  system(paste(shQuote(file.path(R.home("bin"), "R")),
