faecac453158b920f8b36bd99b25c050ea603028
Unknown <marcel.schliebs@sciencespo.fr>
2017-12-18 14:42:02 +0100

    Added empirical dirichlet confidence intervals

---
+++ R/useful_functions.R
@@ -0,0 +1,10 @@
+#' Function %nin%
+#' @description The opposite of %in%
+#' @return Logical Vector
+#' @examples
+#' c(2,4,8) %nin% c(1,4,7)
+#' @section Warning:
+#' Do not operate heavy machinery within 8 hours of using this function.
+#' @export
+`%nin%` <- function(x,y) ! x %in% y
+
